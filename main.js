39241a50aefbdb58147ec412f8843ea64a96e243
Adam Donaghy <donaghyam@gmail.com>
2022-01-12 14:49:31 -0600

    calculator

--- main.js
+++ main.js
@@ -17,4 +17,38 @@
 // 15. Place dough in oven
 // 16. Remove from oven
 
+const subtract = (firstNumber, secondNumber) => {
+    const sub = firstNumber - secondNumber
+    return sub
+}
+
+const resultSub = subtract(9, 4)
+console.log(resultSub)
+
+
+const divide = (firstNumber, secondNumber) => {
+    const divi = firstNumber / secondNumber
+    return divi
+}
+
+const resultDiv = divide(9, 4)
+console.log(resultDiv)
+
+
+const multiply = (firstNumber, secondNumber) => {
+    const mult = firstNumber * secondNumber
+    return mult
+}
+
+const resultMult = multiply(9, 4)
+console.log(resultMult)
+
+
+const square = (firstNumber) => {
+    const sq = firstNumber * firstNumber
+    return sq
+}
+
+const resultSquare = square(9)
+console.log(resultSquare)
 
