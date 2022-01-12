0978486b73559708d3b3afb842327633f061c309
Adam Donaghy <donaghyam@gmail.com>
2022-01-12 14:50:15 -0600

    working calcuator

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-// Task: making bread 
 
-// 1. Check for ingredients
-// 2. Heat water 
-// 3. Weigh water 
-// 4. Weigh yeast 
-// 5. Add yeast to water 
-// 6. Weigh salt
-// 7. Weigh flour
-// 8. Add salt to flour
-// 9. Add yeast/water mix to flour/salt mix
-// 10. Mix until dough forms
-// 11. Cover bowl with cling wrap
-// 12. Let sit out until risen
-// 13. Prehead oven
-// 14. Shape dough
-// 15. Place dough in oven
-// 16. Remove from oven
 
 const subtract = (firstNumber, secondNumber) => {
     const sub = firstNumber - secondNumber
